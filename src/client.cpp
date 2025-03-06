868a0918bfb7cbafe41b4f55bd3a52fd2f656684
Zhengqiang Liao <lzq666amn@gmail.com>
2025-03-06 22:19:08 +0800

    Initial

--- src/client.cpp
+++ src/client.cpp
@@ -1,1 +1,31 @@
 #include "client.h"
+
+
+Client::Client(std::string id, const Server& server) {
+
+}
+
+std::string Client::get_id()
+{
+    return std::string();
+}
+
+std::string Client::get_publickey() {
+    return std::string();
+}
+
+double Client::get_wallet() {
+    return 0.0f;
+}
+
+std::string Client::sign(std::string txt) {
+    return std::string();
+}
+
+bool Client::transfer_money(std::string receiver, double value) {
+    return true;
+}
+
+size_t Client::sgenerate_nonce() {
+    return size_t();
+}
--- src/server.cpp
+++ src/server.cpp
@@ -1,1 +1,39 @@
 #include "server.h"
+
+
+Server::Server() {
+    
+}
+
+std::shared_ptr<Client> Server::add_client(std::string id) {
+
+}
+
+std::shared_ptr<Client> Server::get_client(std::string id) {
+
+}
+
+double Server::get_wallet(std::string id) {
+
+}
+
+bool Server::parse_trx(std::string trx, std::string sender,
+    std::string receiver, double value) {
+
+}
+
+bool Server::add_pending_trx(std::string trx, std::string signature) {
+
+}
+
+size_t Server::mine() {
+    return size_t();
+}
+
+void show_wallets(const Server& server)
+{
+    std::cout << std::string(20, '*') << std::endl;
+    for(const auto& client: server.clients)
+        std::cout << client.first->get_id() <<  " : "  << client.second << std::endl;
+    std::cout << std::string(20, '*') << std::endl;
+}
